62c39dacb5997a60efca3447f34332517fb927e9
LAPTOP-KE6E89HM\rinrada <nxmm19@gmail.com>
2025-02-12 23:31:04 +0700

    Initial commit or update API files

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ import os
     
     
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres1234@lkakaloly.cjq0eaqisx5g.ap-southeast-2.rds.amazonaws.com:5432/Kakaloly')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres1234@lkakaloly.cjq0eaqisx5g.ap-southeast-2.rds.amazonaws.com:5432/postgres')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')  # ค่าคีย์ที่ใช้ในการเข้ารหัส
     LOGIN_VIEW = 'users.login'
